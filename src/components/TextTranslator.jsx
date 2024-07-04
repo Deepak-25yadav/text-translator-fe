81284cd0e1dfa7ef6dae353e71b02b203679a241
Deepak-25yadav <25yadavdeepak@gmail.com>
2024-07-04 14:06:32 +0530

    documentation of code is done

--- src/components/TextTranslator.jsx
+++ src/components/TextTranslator.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import SpeechRecognition, {
-  useSpeechRecognition,
-} from "react-speech-recognition";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 import mic from "../assets/images/mic.svg";
 import stop from "../assets/images/stop.svg";
 import volume from "../assets/images/volume.svg";
@@ -11,34 +9,30 @@ import copyIcon from "../assets/images/copyIcon.svg";
 import "../styles/TextTranslator.css";
 
 const TextTranslator = () => {
-  const [from, setFrom] = useState("en");
-  const [to, setTo] = useState("hi");
-  const [inputText, setInputText] = useState("");
-  const [outputText, setOutputText] = useState("");
-  const [languages, setLanguages] = useState([]);
-  const [outputTextCopied, setOutputTextCopied] = useState(false); // State to track if text has been copied
-  const [inputTextCopied, setInputTextCopied] = useState(false);
+  // State hooks
+  const [from, setFrom] = useState("en"); // Source language
+  const [to, setTo] = useState("hi"); // Target language
+  const [inputText, setInputText] = useState(""); // Input text
+  const [outputText, setOutputText] = useState(""); // Translated text
+  const [languages, setLanguages] = useState([]); // Available languages
+  const [outputTextCopied, setOutputTextCopied] = useState(false); // Output text copy status
+  const [inputTextCopied, setInputTextCopied] = useState(false); // Input text copy status
 
-  const {
-    transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition,
-  } = useSpeechRecognition();
+  // Speech recognition hooks
+  const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition();
 
+  // Effect hook to update input text with transcript
   useEffect(() => {
     if (transcript) {
       setInputText(transcript);
     }
   }, [transcript]);
 
+  // Effect hook to fetch available languages on component mount
   useEffect(() => {
     const fetchLanguages = async () => {
       try {
-        const response = await axios.get(
-          "https://libretranslate.com/languages",
-          { headers: { accept: "application/json" } }
-        );
+        const response = await axios.get("https://libretranslate.com/languages", { headers: { accept: "application/json" } });
         setLanguages(response.data);
       } catch (error) {
         console.log("error", error);
@@ -48,6 +42,7 @@ const TextTranslator = () => {
     fetchLanguages();
   }, []);
 
+  // Translate input text
   const handleTranslate = async () => {
     try {
       console.time("Start Time Translation API Call");
@@ -66,52 +61,56 @@ const TextTranslator = () => {
     }
   };
 
+  // Speak input text
   const handleSourceSpeech = () => {
     let utterance = new SpeechSynthesisUtterance(inputText);
     speechSynthesis.speak(utterance);
   };
 
+  // Speak translated text
   const handleTargetSpeech = () => {
     let utterance = new SpeechSynthesisUtterance(outputText);
     utterance.lang = to;
     speechSynthesis.speak(utterance);
   };
 
+  // Check browser support for speech recognition
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
 
+  // Copy output text to clipboard
   const handleCopyoutputText = () => {
     navigator.clipboard.writeText(outputText).then(() => {
       setOutputTextCopied(true); // Change copy status to true
-
-      // Reset copy status after 1 second
       setTimeout(() => {
-        setOutputTextCopied(false);
+        setOutputTextCopied(false); // Reset copy status after 1 second
       }, 1000);
     });
   };
 
+  // Copy input text to clipboard
   const handleCopyinputText = () => {
     navigator.clipboard.writeText(inputText).then(() => {
       setInputTextCopied(true); // Change copy status to true
-
-      // Reset copy status after 1 second
       setTimeout(() => {
-        setInputTextCopied(false);
+        setInputTextCopied(false); // Reset copy status after 1 second
       }, 1000);
     });
   };
 
+  // Reset input text and transcript
   const handleReset = () => {
     resetTranscript();
     setInputText("");
   };
 
+  // Clear translated text
   const handleClearText = () => {
     setOutputText("");
   };
 
+  // Swap input and output texts and languages
   const handleExchange = () => {
     if (inputText && outputText && to && from) {
       setInputText(outputText);
@@ -122,48 +121,17 @@ const TextTranslator = () => {
   };
 
   return (
-    <div
-      className="translator-container"
-      style={{
-        backgroundColor: "#e6f4fc",
-        borderRadius: "10px",
-        width: "80%",
-        marginTop: "40px",
-      }}
-    >
-      <p
-        className="text-translator-p"
-        style={{
-          color: "#046ede",
-          marginTop: "10px",
-          position: "relative",
-          top: "-20px",
-        }}
-      >
+    <div className="translator-container" style={{ backgroundColor: "#e6f4fc", borderRadius: "10px", width: "80%", marginTop: "40px" }}>
+      <p className="text-translator-p" style={{ color: "#046ede", marginTop: "10px", position: "relative", top: "-20px" }}>
         Punt-Partner Text Translator
       </p>
       <div className="translator-main">
         <div className="translator-column">
           <div className="translator-controls">
-            <img
-              onClick={SpeechRecognition.startListening}
-              width={"20px"}
-              src={mic}
-              alt="Mic"
-            />
-            <img
-              className={listening ? "translator-listening" : ""}
-              onClick={SpeechRecognition.stopListening}
-              src={stop}
-              width={"25px"}
-              alt="Stop"
-            />
+            <img onClick={SpeechRecognition.startListening} width={"20px"} src={mic} alt="Mic" />
+            <img className={listening ? "translator-listening" : ""} onClick={SpeechRecognition.stopListening} src={stop} width={"25px"} alt="Stop" />
             <button onClick={handleReset}>Reset Text</button>
-            <select
-              onChange={(e) => setFrom(e.target.value)}
-              value={from}
-              style={{ color: "#04b0ad" }}
-            >
+            <select onChange={(e) => setFrom(e.target.value)} value={from} style={{ color: "#04b0ad" }}>
               <option value="en">English</option>
               {languages.map((item, i) => (
                 <option key={i} value={item.code}>
@@ -171,56 +139,25 @@ const TextTranslator = () => {
                 </option>
               ))}
             </select>
-            <img
-              onClick={handleSourceSpeech}
-              width={"25px"}
-              src={volume}
-              alt="Volume"
-            />
-            {inputText &&
-              !inputTextCopied && ( // Show copy icon only when there's text and it hasn't been copied
-                <img
-                  onClick={handleCopyinputText}
-                  src={copyIcon}
-                  width={"25px"}
-                  alt="Copy"
-                />
-              )}
+            <img onClick={handleSourceSpeech} width={"25px"} src={volume} alt="Volume" />
+            {inputText && !inputTextCopied && ( // Show copy icon only when there's text and it hasn't been copied
+              <img onClick={handleCopyinputText} src={copyIcon} width={"25px"} alt="Copy" />
+            )}
             {inputTextCopied && ( // Show a message or change icon color when text is copied
-              <span style={{ color: "orange", marginTop: "20px" }}>
-                Copied!
-              </span>
+              <span style={{ color: "orange", marginTop: "20px" }}>Copied!</span>
             )}
           </div>
-          <textarea
-            onChange={(e) => setInputText(e.target.value)}
-            value={inputText}
-            // value={transcript ? transcript : inputText}
-            placeholder="Enter your text here..."
-            rows={15}
-            cols={50}
-            style={{ fontSize: "16px" }}
-          ></textarea>
+          <textarea onChange={(e) => setInputText(e.target.value)} value={inputText} placeholder="Enter your text here..." rows={15} cols={50} style={{ fontSize: "16px" }}></textarea>
         </div>
 
         <button onClick={handleExchange}>
-          <span style={{ position: "relative", top: "7px", fontSize: "14px" }}>
-            Swap
-          </span>
-          <img
-            src={rightArrow}
-            alt="right"
-            style={{ marginBottom: "25px", width: "60px" }}
-          />
+          <span style={{ position: "relative", top: "7px", fontSize: "14px" }}>Swap</span>
+          <img src={rightArrow} alt="right" style={{ marginBottom: "25px", width: "60px" }} />
         </button>
         <div className="translator-column">
           <div className="translator-controls">
             <button onClick={handleClearText}>Clear Text</button>
-            <select
-              onChange={(e) => setTo(e.target.value)}
-              value={to}
-              style={{ color: "#04b0ad" }}
-            >
+            <select onChange={(e) => setTo(e.target.value)} value={to} style={{ color: "#04b0ad" }}>
               <option value="hi">Hindi</option>
               {languages.map((item, i) => (
                 <option key={i} value={item.code}>
@@ -228,45 +165,18 @@ const TextTranslator = () => {
                 </option>
               ))}
             </select>
-            <img
-              onClick={handleTargetSpeech}
-              width={"25px"}
-              src={volume}
-              alt="Volume"
-            />
-            {outputText &&
-              !outputTextCopied && ( // Show copy icon only when there's text and it hasn't been copied
-                <img
-                  onClick={handleCopyoutputText}
-                  src={copyIcon}
-                  width={"25px"}
-                  alt="Copy"
-                />
-              )}
+            <img onClick={handleTargetSpeech} width={"25px"} src={volume} alt="Volume" />
+            {outputText && !outputTextCopied && ( // Show copy icon only when there's text and it hasn't been copied
+              <img onClick={handleCopyoutputText} src={copyIcon} width={"25px"} alt="Copy" />
+            )}
             {outputTextCopied && ( // Show a message or change icon color when text is copied
-              <span style={{ color: "orange", marginTop: "20px" }}>
-                Copied!
-              </span>
+              <span style={{ color: "orange", marginTop: "20px" }}>Copied!</span>
             )}
           </div>
-          <textarea
-            readOnly
-            value={outputText}
-            placeholder="Your Translated Text"
-            rows={15}
-            cols={50}
-            style={{
-              backgroundColor: "#f7fafa",
-              fontSize: "16px",
-              color: "#484bf7",
-            }}
-          ></textarea>
+          <textarea readOnly value={outputText} placeholder="Your Translated Text" rows={15} cols={50} style={{ backgroundColor: "#f7fafa", fontSize: "16px", color: "#484bf7" }}></textarea>
         </div>
       </div>
-      <button
-        onClick={handleTranslate}
-        style={{ width: "150px", height: "40px", fontSize: "20px" }}
-      >
+      <button onClick={handleTranslate} style={{ width: "150px", height: "40px", fontSize: "20px" }}>
         Translate
       </button>
     </div>
