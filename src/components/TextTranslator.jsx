0ab5e640ff15eab721316d086e272cea8ac03c28
Deepak-25yadav <25yadavdeepak@gmail.com>
2024-07-04 12:28:47 +0530

    feat: added copy functionality at inputText side also

--- src/components/TextTranslator.jsx
+++ src/components/TextTranslator.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 import mic from "../assets/images/mic.svg";
-import stop from '../assets/images/stop.svg';
+import stop from "../assets/images/stop.svg";
 import volume from "../assets/images/volume.svg";
 import rightArrow from "../assets/images/rightArrow.svg";
-import copyIcon from "../assets/images/copyIcon.svg"
+import copyIcon from "../assets/images/copyIcon.svg";
 import "../styles/TextTranslator.css";
 
 const TextTranslator = () => {
@@ -14,13 +16,14 @@ const TextTranslator = () => {
   const [inputText, setInputText] = useState("");
   const [outputText, setOutputText] = useState("");
   const [languages, setLanguages] = useState([]);
-  const [copied, setCopied] = useState(false); // State to track if text has been copied
+  const [outputTextCopied, setOutputTextCopied] = useState(false); // State to track if text has been copied
+  const [inputTextCopied, setInputTextCopied] = useState(false);
 
   const {
     transcript,
     listening,
     resetTranscript,
-    browserSupportsSpeechRecognition
+    browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
 
   useEffect(() => {
@@ -47,6 +50,7 @@ const TextTranslator = () => {
 
   const handleTranslate = async () => {
     try {
+      console.time("Start Time Translation API Call");
       const response = await axios.post(
         "https://text-translator-be.onrender.com/translator/translate-input-text",
         {
@@ -55,6 +59,7 @@ const TextTranslator = () => {
           toLang: to,
         }
       );
+      console.timeEnd("End Time Translation API Call");
       setOutputText(response.data.translatedText);
     } catch (error) {
       console.error("Translation error:", error);
@@ -76,62 +81,116 @@ const TextTranslator = () => {
     return <span>Browser doesn't support speech recognition.</span>;
   }
 
-  const handleCopyText = () => {
+  const handleCopyoutputText = () => {
     navigator.clipboard.writeText(outputText).then(() => {
-      setCopied(true); // Change copy status to true
-  
+      setOutputTextCopied(true); // Change copy status to true
+
       // Reset copy status after 1 second
       setTimeout(() => {
-        setCopied(false);
+        setOutputTextCopied(false);
       }, 1000);
     });
   };
 
-const handleReset=()=>{
-    resetTranscript()
-    setInputText("")
-}
+  const handleCopyinputText = () => {
+    navigator.clipboard.writeText(inputText).then(() => {
+      setInputTextCopied(true); // Change copy status to true
 
-const handleClearText=()=>{
-  setOutputText("")
-}
+      // Reset copy status after 1 second
+      setTimeout(() => {
+        setInputTextCopied(false);
+      }, 1000);
+    });
+  };
 
+  const handleReset = () => {
+    resetTranscript();
+    setInputText("");
+  };
 
-// const handleExchange = () => {
-//   if (inputText && outputText && to && from) {
-//     setInputText(outputText);
-//     setOutputText(inputText);
-  
-//   }
-// };
+  const handleClearText = () => {
+    setOutputText("");
+  };
 
-const handleExchange = () => {
-  if (inputText && outputText && to && from) {
-    setInputText(outputText);
-    setOutputText(inputText);
-    setFrom(to);
-    setTo(from);
-  }
-};
-
-
+  const handleExchange = () => {
+    if (inputText && outputText && to && from) {
+      setInputText(outputText);
+      setOutputText(inputText);
+      setFrom(to);
+      setTo(from);
+    }
+  };
 
   return (
-    <div className="translator-container" style={{backgroundColor:"#e6f4fc",borderRadius:"10px",width:"80%", marginTop:"40px"}}>
-    <p className="text-translator-p" style={{color:"#046ede",marginTop:"10px",position:"relative",top:"-20px"}}>Punt-Partner Text Translator</p>
+    <div
+      className="translator-container"
+      style={{
+        backgroundColor: "#e6f4fc",
+        borderRadius: "10px",
+        width: "80%",
+        marginTop: "40px",
+      }}
+    >
+      <p
+        className="text-translator-p"
+        style={{
+          color: "#046ede",
+          marginTop: "10px",
+          position: "relative",
+          top: "-20px",
+        }}
+      >
+        Punt-Partner Text Translator
+      </p>
       <div className="translator-main">
         <div className="translator-column">
           <div className="translator-controls">
-            <img onClick={SpeechRecognition.startListening} width={"20px"} src={mic} alt="Mic" />
-            <img className={listening ? 'translator-listening' : ''} onClick={SpeechRecognition.stopListening} src={stop} width={"25px"} alt="Stop" />
+            <img
+              onClick={SpeechRecognition.startListening}
+              width={"20px"}
+              src={mic}
+              alt="Mic"
+            />
+            <img
+              className={listening ? "translator-listening" : ""}
+              onClick={SpeechRecognition.stopListening}
+              src={stop}
+              width={"25px"}
+              alt="Stop"
+            />
             <button onClick={handleReset}>Reset Text</button>
-            <select onChange={(e) => setFrom(e.target.value)} value={from}  style={{color:"#04b0ad"}} >
+            <select
+              onChange={(e) => setFrom(e.target.value)}
+              value={from}
+              style={{ color: "#04b0ad" }}
+            >
               <option value="en">English</option>
               {languages.map((item, i) => (
-                <option key={i} value={item.code}>{item.name}</option>
+                <option key={i} value={item.code}>
+                  {item.name}
+                </option>
               ))}
             </select>
-            <img onClick={handleSourceSpeech} width={"25px"} src={volume} alt="Volume" />
+            <img
+              onClick={handleSourceSpeech}
+              width={"25px"}
+              src={volume}
+              alt="Volume"
+            />
+            {inputText &&
+              !inputTextCopied && ( // Show copy icon only when there's text and it hasn't been copied
+                <img
+                  onClick={handleCopyinputText}
+                  src={copyIcon}
+                  width={"25px"}
+                  alt="Copy"
+                />
+              )}
+            {inputTextCopied && ( // Show a message or change icon color when text is copied
+              <span style={{ color: "orange", marginTop: "20px" }}>
+                Copied!
+              </span>
+            )}
           </div>
           <textarea
             onChange={(e) => setInputText(e.target.value)}
@@ -140,29 +199,54 @@ const handleExchange = () => {
             placeholder="Enter your text here..."
             rows={15}
             cols={50}
-            style={{fontSize: "16px"}}
+            style={{ fontSize: "16px" }}
           ></textarea>
         </div>
-        
+
         <button onClick={handleExchange}>
-        <span style={{position:"relative", top:"7px", fontSize:"14px"}}>Swap</span> 
-        <img src={rightArrow} alt="right" style={{marginBottom:"25px",width:"60px"}}/>
+          <span style={{ position: "relative", top: "7px", fontSize: "14px" }}>
+            Swap
+          </span>
+          <img
+            src={rightArrow}
+            alt="right"
+            style={{ marginBottom: "25px", width: "60px" }}
+          />
         </button>
         <div className="translator-column">
           <div className="translator-controls">
-          <button onClick={handleClearText}>Clear Text</button>
-            <select onChange={(e) => setTo(e.target.value)} value={to}  style={{color:"#04b0ad"}}>
+            <button onClick={handleClearText}>Clear Text</button>
+            <select
+              onChange={(e) => setTo(e.target.value)}
+              value={to}
+              style={{ color: "#04b0ad" }}
+            >
               <option value="hi">Hindi</option>
               {languages.map((item, i) => (
-                <option key={i} value={item.code}>{item.name}</option>
+                <option key={i} value={item.code}>
+                  {item.name}
+                </option>
               ))}
             </select>
-            <img onClick={handleTargetSpeech} width={"25px"} src={volume} alt="Volume" />
-            {outputText && !copied && ( // Show copy icon only when there's text and it hasn't been copied
-              <img onClick={handleCopyText} src={copyIcon} width={"25px"} alt="Copy" />
-            )}
-            {copied && ( // Show a message or change icon color when text is copied
-              <span style={{ color: "orange" , marginTop:"20px"}}>Copied!</span>
+            <img
+              onClick={handleTargetSpeech}
+              width={"25px"}
+              src={volume}
+              alt="Volume"
+            />
+            {outputText &&
+              !outputTextCopied && ( // Show copy icon only when there's text and it hasn't been copied
+                <img
+                  onClick={handleCopyoutputText}
+                  src={copyIcon}
+                  width={"25px"}
+                  alt="Copy"
+                />
+              )}
+            {outputTextCopied && ( // Show a message or change icon color when text is copied
+              <span style={{ color: "orange", marginTop: "20px" }}>
+                Copied!
+              </span>
             )}
           </div>
           <textarea
@@ -171,11 +255,20 @@ const handleExchange = () => {
             placeholder="Your Translated Text"
             rows={15}
             cols={50}
-            style={{backgroundColor:"#f7fafa", fontSize: "16px", color:"#484bf7"}}
+            style={{
+              backgroundColor: "#f7fafa",
+              fontSize: "16px",
+              color: "#484bf7",
+            }}
           ></textarea>
         </div>
       </div>
-      <button onClick={handleTranslate} style={{width:"150px", height:"40px",fontSize:'20px'}}>Translate</button>
+      <button
+        onClick={handleTranslate}
+        style={{ width: "150px", height: "40px", fontSize: "20px" }}
+      >
+        Translate
+      </button>
     </div>
   );
 };
